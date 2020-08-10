5a607eca94c06759d3ad5738dc885cd41ac31252
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2020-08-10 15:27:14 +0530

    Update infra_manager.py

--- src/terraform/infra_manager.py
+++ src/terraform/infra_manager.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-cmds = ["sudo git clone https://github.com/krishnamaram2/infrastructure_manager.git && cd ./infrastructure_manager/src", "sudo terraform init ./infrastructure_manager/src/ && sudo terraform  validate ./infrastructure_manager/src/  && sudo terraform apply -auto-approve  ./infrastructure_manager/src/"]
+cmds = ["sudo git clone https://github.com/krishnamaram2/infrastructure_manager.git && cd ./infrastructure_manager/src/webapp", "sudo terraform init ./infrastructure_manager/src/webapp/ && sudo terraform  validate ./infrastructure_manager/src/webapp  && sudo terraform apply -auto-approve  ./infrastructure_manager/src/webapp"]
 
 for cmd in cmds:
     print("commnad to execute", cmd)
